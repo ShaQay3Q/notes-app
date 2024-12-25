48a1d5e04f6b7eeacdfc73264de793d189d321cc
ShaQayeQ <shaghay3gh@gmail.com>
2024-12-25 13:46:30 +0100

    start using yargs

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-const add = require("./utils");
+// const add = require("./utils");
 const getNotes = require("./notes");
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(add(3, 4));
+const ADD = "ADD";
+const REMOVE = "REMOVE";
+const READ = "READ";
+const LIST = "LIST";
+
 console.log(getNotes());
 const log = console.log;
 
+// Customized version
+// yargs.version("2.0.0");
+
+//Commands: add, remove, read , list
+yargs.command({
+	command: ADD,
+	describe: "Add a new note",
+	// need a fucntion value so that the command will be exucuted
+	handler: () => {
+		log(chalk.blue.bold("New note has been added."));
+	},
+});
+yargs.command({
+	command: REMOVE,
+	describe: "Remove a note",
+	handler: () => {
+		log(chalk.blue.bold("New note has been added."));
+	},
+});
+yargs.command({
+	command: READ,
+	describe: "Read a note",
+	handler: () => {
+		log(chalk.blue.bold("Note content"));
+	},
+});
+yargs.command({
+	command: LIST,
+	describe: "List all notes",
+	handler: () => {
+		log(chalk.blue.bold("Notes List..."));
+	},
+});
 // console.log(validator.isEmail("shaghay3gh@gmail.com"));
 // console.log(validator.isURL("http://google.com"));
+// const command = process.argv[2];
 
-log(chalk.green.bold("Success!"));
-log(chalk.blue("Hello") + " World" + chalk.red.bold("!"));
-log(chalk.blue.bgRed.inverse.bold("Hello world!"));
-log(chalk.bgBlue.red.inverse.bold("Hello world!"));
-log(chalk.blue.bgRed.bold("Hello world!"));
-
-log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
-
-log(chalk.rgb(200, 45, 67).underline("Underlined reddish color"));
-log(chalk`
-    CPU: ${chalk.red("90%")}
-    RAM: ${chalk.green("40%")}
-    DISK: ${chalk.yellow("70%")}
-    `);
-
-log(chalk.hex("#DEADED").bold("Bold gray!"));
+log(process.argv);
+log(yargs.argv);
--- app01.js
+++ app01.js
@@ -13,3 +13,30 @@ fs.appendFileSync("note.txt", "\nTest the new line!");
 
 // writeFileSync("message.txt", "Line one.\n");
 // appendFileSync("message.txt", "Line two.\n");
+const chalk = require("chalk");
+
+console.log(add(3, 4));
+console.log(getNotes());
+const log = console.log;
+
+// console.log(validator.isEmail("shaghay3gh@gmail.com"));
+// console.log(validator.isURL("http://google.com"));
+
+log(chalk.green.bold("Success!"));
+log(chalk.blue("Hello") + " World" + chalk.red.bold("!"));
+log(chalk.blue.bgRed.inverse.bold("Hello world!"));
+log(chalk.bgBlue.red.inverse.bold("Hello world!"));
+log(chalk.blue.bgRed.bold("Hello world!"));
+
+log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
+
+log(chalk.rgb(200, 45, 67).underline("Underlined reddish color"));
+log(chalk`
+    CPU: ${chalk.red("90%")}
+    RAM: ${chalk.green("40%")}
+    DISK: ${chalk.yellow("70%")}
+    `);
+
+log(chalk.hex("#DEADED").bold("Bold gray!"));
+
+log(process.argv[2]);
