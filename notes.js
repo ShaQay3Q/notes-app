347a6906dc9df705f0d74eff387c0f05d67354e9
ShaQayeQ <shaghay3gh@gmail.com>
2024-12-30 14:55:42 +0100

    add notes by re-writing whole content of a json file

--- notes.js
+++ notes.js
@@ -1,16 +1,34 @@
 const fs = require("fs");
+const { title } = require("process");
 const getNotes = () => {
 	return "Your notes...";
 };
 
+// Load ALL THE NOTES from the file
 const loadNotes = () => {
-	const dataBuffer = fs.readFileSync("note.json"); // if the file isn;t exist => error => try{}catch{}
-	const jsonData = dataBuffer.toString();
-	return JSON.parse(jsonData);
+	try {
+		const dataBuffer = fs.readFileSync("notes.json"); // if the file isn't exist => error => try{}catch{}
+		const jsonData = dataBuffer.toString();
+		return JSON.parse(jsonData);
+	} catch (e) {
+		return [];
+	}
+};
+
+// Save a note => basically RE-WRITE the whole notes!
+const saveNotes = (notes) => {
+	const notesJSON = JSON.stringify(notes);
+	fs.writeFileSync("notes.json", notesJSON);
 };
 
 const addNote = (tile, body) => {
 	const notes = loadNotes();
+	notes.push({
+		// property : value
+		title: tile,
+		body: body,
+	});
+	saveNotes(notes);
 };
 
 module.exports = {
